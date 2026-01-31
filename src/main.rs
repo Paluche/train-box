804e2001973745bb43793643d73a33ca563b6a47
Hubert Lefevre <hubelefevre@gmail.com>
2026-01-31 15:06:24 +0100

    Train control example cross compiled

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+use brio_smart_tech::{BrioSmartTech, Color};
+use btleplug::{api::Manager as _, platform::Manager};
+use strum::IntoEnumIterator;
+use tokio::time::{Duration, sleep};
+
+#[tokio::main]
+async fn main()  -> Result<(), Box<dyn Error>> {
+    println!("Initializing BLE manager");
+    let manager = Manager::new().await.unwrap();
+    // Get the first bluetooth adapter
+    let adapters = manager.adapters().await.unwrap();
+    let central = adapters.first().unwrap();
+
+    println!("Searching for train");
+    let train = BrioSmartTech::new(central)
+        .await?
+        .expect("device not found");
+
+    println!("Sending different colors");
+    for c in Color::iter() {
+        println!("Color {c:?}");
+        for i in 0..16 {
+            train.set_color(c, i).await?;
+            sleep(Duration::from_millis(100)).await;
+        }
+    }
+
+    train.set_color(Color::White, 15).await?;
+    sleep(Duration::from_millis(300)).await;
+
+    println!("Forward");
+    train.forward(7).await?;
+    sleep(Duration::from_secs(10)).await;
+
+    println!("Backward");
+    train.backward(7).await?;
+    sleep(Duration::from_secs(1)).await;
+
+    println!("Stop");
+    train.stop().await?;
+    sleep(Duration::from_millis(300)).await;
+
+    Ok(())
 }
